8aa9a6982b5e1d6fa4923f17ed3a08149ae38d74
shoaibrrian <shoaibrian@gmail.com>
2025-08-28 15:32:37 +0600

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-6xl bg-red-200'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
