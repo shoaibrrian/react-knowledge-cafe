216ca9bd57bf50a960f8475261e39fd55f999230
shoaibrrian <shoaibrian@gmail.com>
2025-08-28 17:46:44 +0600

    display blog data and hashtags

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,7 +1,26 @@
 const Blog = ({blog}) => {
+    const {title, cover_pic, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover_pic} alt={`Cover picture of the title ${title}`} />
+            <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                    <img className="w-16" src={author_img} alt="" />
+                    <div className="ml-6">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}> <a href="">{hash}</a> </span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
+import Blog from "../blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
     }, [])
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length} </h1>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                    ></Blog>)
+            }
         </div>
     );
 };
